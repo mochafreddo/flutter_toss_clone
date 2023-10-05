1d1f8f1b81d1d1325bcc768490fa24a44197e669
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-10-05 18:40:55 +0900

    18. 오픈 소스 화면 및 마무리

--- lib/screen/main/tab/stock/setting/s_setting.dart
+++ lib/screen/main/tab/stock/setting/s_setting.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
+import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/dart/extension/datetime_extension.dart';
-import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
 import 'package:fast_app_base/common/data/preference/prefs.dart';
 import 'package:fast_app_base/common/widget/w_big_button.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/d_number.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/w_switch_menu.dart';
+import 'package:fast_app_base/screen/opensource/s_opensource.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -44,7 +45,10 @@ class _SettingScreenState extends State<SettingScreen> {
                 if (number != null) {
                   Prefs.number.set(number);
                 }
-              }))
+              })),
+          BigButton("오픈소스 화면", onTap: () async {
+            Nav.push(const OpensourceScreen());
+          })
         ]));
   }
 }
--- lib/screen/opensource/s_opensource.dart
+++ lib/screen/opensource/s_opensource.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/material.dart';
+
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/util/local_json.dart';
 import 'package:fast_app_base/screen/opensource/vo_package.dart';
-import 'package:flutter/material.dart';
-
-import 'w_opensource_item.dart';
+import 'package:fast_app_base/screen/opensource/w_opensource_item.dart';
 
 /// 아래의 명령어를 통해서, 주기적으로 라이센스 json을 최신화 해주세요.
 /// flutter pub run flutter_oss_licenses:generate.dart -o assets/json/licenses.json --json
@@ -25,24 +25,17 @@ class _OpensourceScreenState extends State<OpensourceScreen> {
 
   void initData() async {
     final list = await LocalJson.getObjectList<Package>("json/licenses.json");
-    setState(() {
-      packageList = list;
-    });
+    setState(() => packageList = list);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: 'opensource'.tr().text.make(),
-      ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => OpensourceItem(packageList[index]),
-        itemCount: packageList.length,
-        separatorBuilder: (BuildContext context, int index) {
-          return const Line().pSymmetric(h:20);
-        },
-      ),
-    );
+        appBar: AppBar(title: 'opensource'.tr().text.make()),
+        body: ListView.separated(
+            itemBuilder: (context, index) => OpensourceItem(packageList[index]),
+            itemCount: packageList.length,
+            separatorBuilder: (BuildContext context, int index) =>
+                const Line().pSymmetric(h: 20)));
   }
 }
